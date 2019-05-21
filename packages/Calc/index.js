de9f17e5a91fd65cb293167291488cc4c8f49784
unknown <abdusidd33@gmail.com>
2019-05-21 03:14:10 +0100

    Initial commit

---
+++ packages/Calc/index.js
@@ -0,0 +1,7 @@
+var add = require("AdditionModule");
+var subtract = require("SubtractionModule");
+
+var sum = add.add(2, 3);
+var diff = subtract.subtract(3, 2);
+
+console.log("Sum: " + sum + " Diff: " + diff);
